361b68fa857bdb15b9481bf8a77014a94b902d8c
Damien Castelltort <dcastelltort@gmail.com>
2017-11-16 14:16:05 +0100

    feat(CLI) : WIP added clap dependencies + initial work with cli processing

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 extern crate byteorder;
 extern crate num;
 
+extern crate clap;
+use clap::{Arg, App, SubCommand};
+
 mod bitmap;
 mod rgb;
 mod mandelbrot;
@@ -8,15 +11,48 @@ mod zoom;
 mod fractalcreator;
 
 fn main() {
-    
-    let mut fractal_reator = fractalcreator::FractalCreator::new(800, 600);
+    let matches = App::new("Fractal Creator")
+                          .version("0.1")
+                          .author("Damien Castelltort")
+                          .about("Generates some mandelbrot fractal")
+						  .arg(Arg::with_name("width")
+						  	.short("w")
+							.long("width")
+							.takes_value(true)
+							//.required(true)
+							.default_value("800")
+						  )
+						  .arg(Arg::with_name("height")
+						  	.short("h")
+							.long("height")
+							.takes_value(true)
+							//.required(true)
+							.default_value("600")
+						  )
+						  .arg(Arg::with_name("output_file")
+						  	.short("of")
+							.long("output_file")
+							.takes_value(true)
+							//.required(true)
+							.default_value("test.bmp")
+						  )
+						  .get_matches();
 
-	fractal_reator.add_range(0.0, rgb::RGB::new(0.0, 0.0, 0.0));
-	fractal_reator.add_range(0.3, rgb::RGB::new(255.0, 0.0, 0.0));
-	fractal_reator.add_range(0.5, rgb::RGB::new(255.0, 255.0, 0.0));
-	fractal_reator.add_range(1.0, rgb::RGB::new(255.0, 255.0, 255.0));
+		println!("{:?}", matches.value_of("width").unwrap());
+		println!("{:?}", matches.value_of("height").unwrap());
+		println!("{:?}", matches.value_of("output_file").unwrap());
 
-	fractal_reator.add_zoom(zoom::Zoom::new(295, 202, 0.1));
-	fractal_reator.add_zoom(zoom::Zoom::new(312, 304, 0.1));
-	fractal_reator.run(String::from("test.bmp"));
+	if false {
+
+		let mut fractal_reator = fractalcreator::FractalCreator::new(800, 600);
+
+		fractal_reator.add_range(0.0, rgb::RGB::new(0.0, 0.0, 0.0));
+		fractal_reator.add_range(0.3, rgb::RGB::new(255.0, 0.0, 0.0));
+		fractal_reator.add_range(0.5, rgb::RGB::new(255.0, 255.0, 0.0));
+		fractal_reator.add_range(1.0, rgb::RGB::new(255.0, 255.0, 255.0));
+
+		fractal_reator.add_zoom(zoom::Zoom::new(295, 202, 0.1));
+		fractal_reator.add_zoom(zoom::Zoom::new(312, 304, 0.1));
+		fractal_reator.run(String::from("test.bmp"));
+	}
 }
