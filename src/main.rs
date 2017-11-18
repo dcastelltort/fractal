66efcddee036904a010f49911d2e8784f92f8ce5
Damien Castelltort <dcastelltort@gmail.com>
2017-11-18 21:04:21 +0100

    feat(cli): basic command line parsing and error checking

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ extern crate num;
 
 extern crate clap;
 use clap::{Arg, App, SubCommand};
+use std::process;
 
 mod bitmap;
 mod rgb;
@@ -11,7 +12,7 @@ mod zoom;
 mod fractalcreator;
 
 fn main() {
-    /*let matches = App::new("Fractal Creator")
+    let matches = App::new("Fractal Creator")
                           .version("0.1")
                           .author("Damien Castelltort")
                           .about("Generates some mandelbrot fractal")
@@ -19,33 +20,49 @@ fn main() {
 						  	.short("w")
 							.long("width")
 							.takes_value(true)
-							//.required(true)
-							.default_value("800")
+							.required(true)
 						  )
 						  .arg(Arg::with_name("height")
 						  	.short("h")
 							.long("height")
 							.takes_value(true)
-							//.required(true)
-							.default_value("600")
+							.required(true)
 						  )
 						  .arg(Arg::with_name("output_file")
 						  	.short("of")
 							.long("output_file")
 							.takes_value(true)
-							//.required(true)
-							.default_value("test.bmp")
+							.required(true)
 						  )
 						  .get_matches();
 
-		println!("{:?}", matches.value_of("width").unwrap());
-		println!("{:?}", matches.value_of("height").unwrap());
-		println!("{:?}", matches.value_of("output_file").unwrap());
-*/
-    let bFlag = true;
-	if bFlag {
+		let width = match str::parse::<u32>(matches.value_of("width").unwrap_or("")) {
+            Ok(n) => n,
+            Err(e) => {
+                println!("failed parsing width, {:?}", e);
+                0
+            }
+        };
 
-		let mut fractal_reator = fractalcreator::FractalCreator::new(800, 600);
+        let height = match str::parse::<u32>(matches.value_of("height").unwrap_or("")) {
+            Ok(n) => n,
+            Err(e) => {
+                println!("failed parsing height, {:?}", e);
+                0
+            }
+        };
+        let output_file = matches.value_of("output_file").unwrap_or("");
+
+        if width == 0 || height == 0 {
+            println!("invalid width/heigh");
+            std::process::exit(1);
+        }
+        if output_file.is_empty() {
+            println!("invalid output file");
+            std::process::exit(1);
+        }
+
+		let mut fractal_reator = fractalcreator::FractalCreator::new(width as i32, height as i32);
 
 		fractal_reator.add_range(0.0, rgb::RGB::new(0.0, 0.0, 0.0));
 		fractal_reator.add_range(0.3, rgb::RGB::new(255.0, 0.0, 0.0));
@@ -54,6 +71,5 @@ fn main() {
 
 		fractal_reator.add_zoom(zoom::Zoom::new(295, 202, 0.1));
 		fractal_reator.add_zoom(zoom::Zoom::new(312, 304, 0.1));
-		fractal_reator.run(String::from("test.bmp"));
-	}
+		fractal_reator.run(String::from(output_file));
 }
